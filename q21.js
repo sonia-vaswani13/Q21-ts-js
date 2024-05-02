35afb25999058fcaf623bae8d6b6fc305563e13e
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-05-02 10:48:00 -0700

    Q 21 ts/js node js of 45 assignments
    Q 21 ts/js node js of 45 assignments

---
+++ q21.js
@@ -0,0 +1,3 @@
+//Q21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var person = { name: "sonia", fname: "Mangoo Ram", age: 29 };
+console.log(person);
